b63c7e6bfc2d0e0e6528943b6ed630fc6972f98e
Tony <tony@clarity.lan>
2020-01-09 17:40:41 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+// Express set up
+let port = process.env.PORT || 3000;
+app.use(express.urlencoded({ extended: true }));
+
+// Start express server
+app.listen(port, () => console.log('Server started on port ' + port));
