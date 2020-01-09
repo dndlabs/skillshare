dd164eee79b9b4cb408e6776ceba31bd0bc2af24
Tony <tony@clarity.lan>
2020-01-09 18:19:25 -0500

    added user model

---
+++ src/models/userModel.js
@@ -0,0 +1,70 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+let userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        minlength: 5
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 7
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        validate(value) {
+            if(!validator.isEmail(value)){
+                throw new Error('Email is invalid');
+            }
+        }
+    },
+    tokens: [{
+        token: String,
+        required: true
+    }]
+})
+
+// Generate a user token to track sessions 
+userSchema.methods.generateToken = async function () {
+    const user = this;
+    const token = jwt.sign({ _id: user._id.toString() }, 'secretkeygoeshere');
+    user.tokens = user.tokens.concat({ token: token });
+    await user.save();
+    return token;
+}
+
+// Find user by credentials for login
+userSchema.statics.findByCredentials = async (username, password) => {
+    const user = await User.findOne({ username: username });
+    if (!user) {
+        throw new Error('Unable to login');
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+        throw new Error('Unable to login');
+    }
+    return user;
+}
+
+
+// Hash the plaintext password before saving to db
+userSchema.pre('save', async function(next) {
+    const user = this;
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8);
+    }
+    next();
+})
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
