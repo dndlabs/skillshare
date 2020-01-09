80ec08efd2f867d959e10e4e829ef9f511b036ed
Tony <tony@clarity.lan>
2020-01-09 17:57:50 -0500

    added db connection and some folders for project structure

---
+++ src/db/mongoose.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+let dburl = process.env.DBURL || 'mongodb://localhost:27017/skillshare_db';
+
+try {
+    mongoose.connect(dburl, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+} catch (error) {
+    console.log(error);
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+require('./db/mongoose');
 
 // Express set up
 let port = process.env.PORT || 3000;
