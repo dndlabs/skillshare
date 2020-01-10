0e1284d6868c196c9f2529fe87a56df7d135e5ce
Tony <tony@clarity.lan>
2020-01-10 13:14:18 -0500

    Added user router import

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const userRouter = require('./routers/userRouter');
 require('./db/mongoose');
 
 // Express set up
 let port = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
+app.use(userRouter);
 
 // Start express server
 app.listen(port, () => console.log('Server started on port ' + port));
