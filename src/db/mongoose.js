a3957a0958f63cdb067ea5ab619bf2bf708ad3ff
Tony <tony@clarity.lan>
2020-01-10 13:14:46 -0500

    Update mongoose options

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -5,7 +5,8 @@ let dburl = process.env.DBURL || 'mongodb://localhost:27017/skillshare_db';
 try {
     mongoose.connect(dburl, {
         useNewUrlParser: true,
-        useUnifiedTopology: true
+        useUnifiedTopology: true,
+        useCreateIndex: true
     })
 } catch (error) {
     console.log(error);
