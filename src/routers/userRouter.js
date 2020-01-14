fb2b7e92373ce05a3feec2ccada375c3d293567f
Tony <tony@clarity.lan>
2020-01-14 12:43:08 -0500

    Added user logout route

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -7,7 +7,7 @@ router.post('/users/register', async (req, res) => {
     let user = new User(req.body);
     try {
         let token = await user.generateToken();
-        res.status(201).send(user + token);
+        res.status(201).send({user, token});
     } catch (e) {
         res.status(400).send(e);
     }
@@ -15,19 +15,35 @@ router.post('/users/register', async (req, res) => {
 
 router.post('/users/login', async (req, res) => {
     try {
+        console.log(req.body.username, req.body.password)
         let user = await User.findByCredentials(req.body.username, req.body.password);
+       
         let token = await user.generateToken();
-        res.send('User logged in' + token)
+        res.send({token})
     } catch (e) {
         res.status(500).send();
     }
 })
 
-router.post('/users/logout', async (req, res) => {
+router.post('/users/logout', auth, async (req, res) => {
     try {
-
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token;
+        });
+        await req.user.save();
+        res.send();
     } catch (e) {
-        
+        res.status(500).send();
+    }
+})
+
+router.post('/users/logoutall', auth, async(req, res) => {
+    try {
+        req.user.tokens = [];
+        await req.user.save();
+        res.send();
+    } catch (e) {
+        res.status(500).send(e);
     }
 })
 
