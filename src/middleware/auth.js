308f6fce3763981e2e0e11e0c62970372ba552eb
Tony <tony@clarity.lan>
2020-01-14 10:09:10 -0500

    Added user verification

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,7 +1,22 @@
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const User = require('../models/userModel');
 
 const auth = async (req, res, next) => {
-
+    try {
+        const token = req.header('Authorization').replace('Bearer ', '');
+        const decoded = jwt.verify(token, 'secretkeygoeshere');
+        const user = await User.findById({ _id: decoded._id, 'tokens.token': token });
+        if(!user) {
+            throw new Error();
+        }
+        req.token = token;
+        req.user = user;
+        // const user = await User.findById()
+        next();
+    } catch (e) {
+        res.status(401).send({ error: 'Please authenticate' + e });
+    }
 }
 
 module.exports = auth;
